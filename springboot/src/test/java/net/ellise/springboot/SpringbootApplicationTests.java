f93f452881a8383cd906c688c2700777e7fb4b83
Edward Ellis <edwardellis.egre@gmail.com>
2025-02-27 16:38:58 +0000

    Springboot builds with connection to postgres database named database.

--- springboot/src/test/java/net/ellise/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/net/ellise/springboot/SpringbootApplicationTests.java
@@ -1,10 +1,27 @@
 package net.ellise.springboot;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine")
+			.withDatabaseName("postgres")
+			.withUsername("postgres")
+			.withPassword("secret");
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
 
 	@Test
 	void contextLoads() {
